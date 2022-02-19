758477d35cee918e5c57db2953e00e6b02dda4dd
René Müller <mail@rene-mueller.xyz>
2022-02-19 18:12:55 +0100

    added better .d.ts file (from compiler)

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,22 +1,44 @@
-import capacitorStorage from "../../../src/store/capacitorStorage";
-
-declare namespace CapacitorStorage {
-
-  export interface CapacitorStorage {
-    getItem(key: string) : object | string
-    setItem(key: string, value: any): void
-    removeItem(key: string) : void
-    getAllKeys() : object | string
-    clear() : void
-  }
-
+declare var _default: CapacitorStorage;
+export default _default;
+declare class CapacitorStorage {
+    /**
+     * The value from storage associated with the given key.
+     *
+     * @since 1.0.0
+     * @param {string} key
+     * @returns {Promise<string | null>}
+     */
+    getItem(key: string): Promise<string | null>;
+    /**
+     * Set the value in storage for a given key.
+     *
+     * @since 1.0.0
+     * @param {string} key
+     * @param {string} value
+     * @returns {Promise<void>}
+     */
+    setItem(key: string, value: string): Promise<void>;
+    /**
+     * Remove the value from storage for a given key, if any.
+     *
+     * @since 1.0.0
+     * @param {string} key
+     * @returns {Promise<void>}
+     */
+    removeItem(key: string): Promise<void>;
+    /**
+     * Return the list of known keys in storage.
+     *
+     * @since 1.0.0
+     * @returns {Promise<string[] | undefined>}
+     */
+    getAllKeys(): Promise<string[] | undefined>;
+    /**
+     * Clear keys and values from storage.
+     *
+     * @since 1.0.0
+     * @returns {Promise<void>}
+     */
+    clear(): Promise<void>;
 }
-
-declare module CapacitorStorage {
-
-  import C = CapacitorStorage.CapacitorStorage;
-  export = C;
-
-}
-
-export default capacitorStorage;
+//# sourceMappingURL=index.d.ts.map
